b7af8694937bc41ef459a7864998c7af4f224c73
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-05-29 20:28:57 -0400

    Fixed env var name bug.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as flows_router
+import os
 
 config = dotenv_values(".env")
 
@@ -9,8 +10,10 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
+    # app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.mongodb_client = os.getenv('ATLAS_URI')
+    # app.database = app.mongodb_client[config["DB_NAME"]]
+    app.database = os.getenv('DB_NAME')
 
 @app.on_event("shutdown")
 def shutdown_db_client():
